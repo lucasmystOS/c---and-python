25dcef49d0353bfa613e1202598b409758a06229
LucasmystOS <lucasdahoravieira10@gmail.com>
2025-05-02 02:41:13 -0300

    first commit

---
+++ base.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Digite um número: ";
+    int n;
+    std::cin >> n;
+    std::cout << "O número digitado foi: " << n << std::endl;
+
+    return 0;
+}
---
+++ base.py
@@ -0,0 +1,2 @@
+valor = input("Digite um valor: ")
+print(f"Você digitou: {valor}")
